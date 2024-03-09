c18fea9ca3910328f8d114234805da305f878e1e
goldpulpy <sawsani1928@gmail.com>
2024-03-09 18:42:49 +0300

    update docker starter

--- docker.sh
+++ docker.sh
@@ -1,7 +1,5 @@
-$service_name = "telegrambot"
+$service_name = "geminibot"
 
 docker rm -f $service_name && docker rmi -f $service_name
 docker build -t $service_name .
-docker run -d --name $service_name \
-           -v ./chat_history.json:/app/chat_history.json \
-           $service_name
+docker run -d --name $service_name $service_name
